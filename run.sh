56e0ac6715ab53e9247514f00df51fa1460af254
Giacomo Carugati <giacomo.carugati@mail.polimi.it>
2024-12-17 23:10:42 +0100

    Validate test-inputs

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ clear
 
 cd build
 
-./verilog_to_json ../test-inputs/xorInPlace.sv
+./verilog_to_json ../test-inputs/arrays.sv
 
 exit_code=$?
 
---
+++ test-inputs/and.sv
@@ -0,0 +1,5 @@
+module And(
+    input logic a, b,
+    output logic out);
+  assign out = a & b;
+endmodule
