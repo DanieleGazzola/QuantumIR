45ad4fe95e86304e1da39f3f9583d680a98d105e
filippo <filippo.buda@mail.polimi.it>
2024-12-18 12:46:21 +0100

    Aggiunti test, run.sh outputta in file, simulate.ipynb cambiati output

--- run.sh
+++ run.sh
@@ -4,14 +4,18 @@ clear
 
 cd build
 
-./verilog_to_json ../test-inputs/arrays.sv
+totest=and
+
+rm -f ../test-outputs/${totest}.out
+
+./verilog_to_json ../test-inputs/$totest.sv >> ../test-outputs/${totest}.out
 
 exit_code=$?
 
 if [ $exit_code -eq 0 ]; then
         cd ..
 
-        python3 main.py
+        python3 main.py >> test-outputs/${totest}.out
 else
     echo "Error in SLANG compilation."
     exit $exit_code  
